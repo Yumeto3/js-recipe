ccb53d1fe8d07c2d9e3efe302b3aafb6ce23a723
Yumeto3 <yumetouchida@gmail.com>
2021-07-17 19:25:40 +0900

    自販機つくる

--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -1,12 +1,28 @@
 // 引数 number を受け取る関数
-const genkiFunction = function(number) {
-  // n を 1 から、 number まで、 値を 1 ずつ増やしながら、 for の {} の中身を繰り返す
-  for (let n = 1; n <= number; n++) {
-    if (n % 3 === 0) {
-      console.log(`${n}!!!!!!!`)
-    } else {
-      console.log(n)
-    }
-  }
+
+const display = document.getElementById("display")
+const button = document.getElementById("button")
+
+const number = 100
+
+button.onclick = function(){
+  // 10ms ごとに countUp を実行するように登録する
+  setInterval(countUp, 10)
+  button.textContent = "stop"
 }
 
+const countUp = function(){
+  count += 1
+  console.log(count) // 追加
+  display.textContent = count/100
+
+  for (let n = 1; n<= number; n++) {
+    if (n%3 === 0) {
+      console.log (n+ "!!!!!!!");
+    }
+    else {
+      console.log (n)
+  }
+  }
+
+}
